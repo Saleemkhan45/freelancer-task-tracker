16a3bcc720bd242e6c6d92e38b834fdfc74030c2
saleemkhan45 <saleemsherzad45@gmail.com>
2026-01-08 11:06:24 +0500

    setup react project with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 text-gray-900">
+      <h1 className="text-3xl font-bold text-center mt-10">
+        Freelancer Tracker App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
